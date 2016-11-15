df3fdff68143446b68dc59e0723448a530557e76
emmy41124 <loveflorescence@gmail.com>
2016-11-15 14:18:56 +0800

    9x9 multiplication table with for

--- commit practice.cpp
+++ commit practice.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main() {
-  cout << "Hello, world!" << endl;
+  int array[9][9];
+
+  for (int i = 1; i < 10; i ++) {
+    for (int j = 1; j < 10; j++) {
+      array[j-1][i-1] = i * j;
+    }
+  }
+
+  for (int i = 1; i < 10; i ++) {
+    for (int j = 1; j < 10; j++) {
+      cout << array[j-1][i-1] << " ";
+    }
+    cout << endl;
+  }
+
 }
